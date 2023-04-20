e99a65ebe58ed57db8b23efb1f6f985badc337e6
Vladimir <120676027+VladimirVikB@users.noreply.github.com>
2023-04-20 21:33:36 +0300

    create

---
+++ C5.2.1.js
@@ -0,0 +1,35 @@
+const xml = `<list>
+  <student>
+    <name lang="en">
+      <first>Ivan</first>
+      <second>Ivanov</second>
+    </name>
+    <age>35</age>
+    <prof>teacher</prof>
+  </student>
+  <student>
+    <name lang="ru">
+      <first>Петр</first>
+      <second>Петров</second>
+    </name>
+    <age>58</age>
+    <prof>driver</prof>
+  </student>
+</list>`;
+
+const parser = new DOMParser();
+const xmlString = parser.parseFromString(xml, 'text/xml');
+
+const students = [];
+const studentElements = xmlString.querySelectorAll('student');
+for (let i = 0; i < studentElements.length; i++) {
+  const student = studentElements[i];
+  const name = `${student.querySelector('first').textContent} ${student.querySelector('second').textContent}`;
+  const age = Number(student.querySelector('age').textContent);
+  const prof = student.querySelector('prof').textContent;
+  const lang = student.querySelector('name').getAttribute('lang');
+  students.push({ name, age, prof, lang });
+}
+
+const obj = { list: students };
+console.log(obj);
