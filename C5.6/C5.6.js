e7b3c0ada52a13f7e07c2c78f7fbf26a40d8e7e9
Vladimir <120676027+VladimirVikB@users.noreply.github.com>
2023-04-27 14:50:57 +0300

    add 5.6

---
+++ C5.6/C5.6.js
@@ -0,0 +1,41 @@
+
+const input1 = document.querySelector('#input1');
+const input2 = document.querySelector('#input2');
+const button = document.querySelector('#button');
+const clearButton = document.querySelector('#clearButton');
+const result = document.querySelector('#result');
+
+function checkInput(num) {
+  return num >= 100 && num <= 300;
+}
+
+function handleClick() {
+
+  const num1 = Number(input1.value);
+  const num2 = Number(input2.value);
+
+
+  if (isNaN(num1) || isNaN(num2) || !checkInput(num1) || !checkInput(num2)) {
+    result.innerHTML = 'Одно из чисел вне диапазона от 100 до 300';
+  } else {
+    const url = `https://picsum.photos/${num1}/${num2}`;
+    fetch(url)
+      .then(response => {
+        const img = document.createElement('img');
+        img.src = response.url;
+        result.appendChild(img);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+}
+
+function clearForm() {
+  input1.value = '';
+  input2.value = '';
+  result.innerHTML = '';
+}
+
+button.addEventListener('click', handleClick);
+clearButton.addEventListener('click', clearForm);
